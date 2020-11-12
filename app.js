73cf89ecbaeb888743141b14d923603a6cdf775b
Raiden Skala <trsraiden@gmail.com>
2020-11-12 14:07:38 -0500

    Complete Challenge 18

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 //jshint esversion:6
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+
+// lodash
+const _ = require('lodash');
+const array = require('lodash/array');
+const object = require('lodash/fp/object');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -13,7 +18,7 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 let posts = [];
 
@@ -49,7 +54,9 @@ app.post("/compose", function(req, res){
 
 app.get("/posts/:postName", function (req, res){
   posts.forEach(function(post){
-    if (post.title === req.params.postName){
+    console.log(_.lowerCase(post.title));
+    console.log(_.lowerCase(req.params.postName));
+    if (_.lowerCase(post.title) === _.lowerCase(req.params.postName)){
       console.log("Match Found!");
     }
   });
